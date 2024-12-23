3fc912424e8c28cceb0093b68cef1dfd3f9e440f
MalcolmX2145 <ekajjamalcolm@gmail.com>
2024-12-23 12:49:25 +0300

    Done with the UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import uuid from 'react-uuid'; 
 import './App.css';
 import Sidebar from './components/Sidebar';
 import Main from './components/Main';
 
-
-
 const App = () => {
   // states.
-const [notes, setNotes] = useState([]);
+const [notes, setNotes] = useState(localStorage.notes ? JSON.parse(localStorage.notes) : []);
 const [activeNote, setActiveNote] = useState(false);
+
+// store the data in browser
+ useEffect(() => {
+  localStorage.setItem("notes", JSON.stringify(notes));
+}, [notes]);
 
 // function for adding a new note
 const onAddNote = () => {
@@ -29,16 +32,29 @@ const onDeleteNote = (idToDelete) => {
   setNotes(notes.filter((note) => note.id !== idToDelete)); 
 }
 
+// function to give the Main component access the the active note stored in state
 const getActiveNote = () => {
   return notes.find((note) => note.id === activeNote);
 }
 
+// function to update an active note
+const onUpdateNote = (updatedNote) => {
+    const updatedNotesArr = notes.map((note) => {
+      if (note.id === updatedNote.id) {
+        return updatedNote;
+      }
+
+      return note;
+    });
+
+    setNotes(updatedNotesArr);
+  };
   return (
     <div className="App">
       <Sidebar notes={notes} onAddNote={onAddNote} onDeleteNote={onDeleteNote} activeNote={activeNote} setActiveNote={setActiveNote}/>
-      <Main activeNote={getActiveNote()}/>
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote}/>
     </div>
   )
 }
 
-export default App
+export default App;
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,5 +1,8 @@
 
 const Sidebar = ({notes, onAddNote, onDeleteNote, setActiveNote, activeNote}) => {
+  // Sorting the notes from descending order (last updated at the top)
+  const sortedNotes = notes.sort((a, b) => b.lastModified - a.lastModified);
+
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
@@ -8,7 +11,7 @@ const Sidebar = ({notes, onAddNote, onDeleteNote, setActiveNote, activeNote}) =>
       </div>
 
       <div className="app-sidebar-notes">
-      {notes.map((note) => (
+      {sortedNotes.map((note) => (
         <div className={`app-sidebar-note ${note.id === activeNote && "active"}`} key={note.id} onClick={() => setActiveNote(note.id)}>
           <div className="sidebar-note-title">
             <strong>{note.title}</strong>
